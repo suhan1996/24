7c7a1352e394c9ae24428f21c773f01e925dad1b
Han Su <hs2657@nyu.edu>
2017-04-13 19:34:55 -0400

    Update research.js

--- research.js
+++ research.js
@@ -60,12 +60,12 @@ var demo = new Vue({
   el: '#demo',
   data: {
     searchQuery: '',
-    gridColumns: ['name', 'power'],
+    gridColumns: ['name', 'time'],
     gridData: [
-      { name: 'Chuck Norris', power: Infinity },
-      { name: 'Bruce Lee', power: 9000 },
-      { name: 'Jackie Chan', power: 7000 },
-      { name: 'Jet Li', power: 8000 }
+      { name: 'Sh', power: 64 },
+      { name: 'Ziming', power: 104 },
+      { name: 'Jackie', power: 150 },
+      { name: 'Lee', power: 180 }
     ]
   }
 })
