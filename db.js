6b5d7fd38d72f684b2eff5f107b7e464c8da930b
suhan1996 <hs2657@nyu.edu>
2017-04-29 12:05:36 -0400

    update

--- db.js
+++ db.js
@@ -39,23 +39,23 @@ mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost/final');
 
 // is the environment variable, NODE_ENV, set to PRODUCTION?
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    // if we're in PRODUCTION mode, then read the configration from a file
-    // use blocking file io to do this...
-    var fs = require('fs');
-    var path = require('path');
-    var fn = path.join(__dirname, 'config.json');
-    var data = fs.readFileSync(fn);
+//if (process.env.NODE_ENV === 'PRODUCTION') {
+//    // if we're in PRODUCTION mode, then read the configration from a file
+//    // use blocking file io to do this...
+//    var fs = require('fs');
+//    var path = require('path');
+//    var fn = path.join(__dirname, 'config.json');
+//    var data = fs.readFileSync(fn);
+//
+//    // our configuration file will be in json, so parse it and set the
+//    // conenction string appropriately!
+//    var conf = JSON.parse(data);
+//    var dbconf = conf.dbconf;
+//} else {
+//    // if we're not in PRODUCTION mode, then use
+//    dbconf = 'mongodb://localhost/final';
+//    console.log("else",dbconf)
+//}
+//console.log("final",dbconf)
 
-    // our configuration file will be in json, so parse it and set the
-    // conenction string appropriately!
-    var conf = JSON.parse(data);
-    var dbconf = conf.dbconf;
-} else {
-    // if we're not in PRODUCTION mode, then use
-    dbconf = 'mongodb://localhost/final';
-    console.log("else",dbconf)
-}
-console.log("final",dbconf)
-
-mongoose.connect(dbconf)
+mongoose.connect('mongodb://suhan1996:112496@ds123361.mlab.com:23361/heroku_mzmc3zf1');
